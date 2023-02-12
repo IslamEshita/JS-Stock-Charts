a64212c4821af6a9e010ab916d2af94df2583df8
Eshita Islam <islam.eshita@gmail.com>
2023-02-12 09:52:42 -0800

    Followed instructions to generate stock chart

--- public/index.js
+++ public/index.js
@@ -4,23 +4,40 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    // Creating an array of stock data per instructions
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
 
+    function getColor(stock) {
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    
+
+    // Adding the chart
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',                   
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                ],
-            }]
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
         }
     });
+    
     
 
 }
